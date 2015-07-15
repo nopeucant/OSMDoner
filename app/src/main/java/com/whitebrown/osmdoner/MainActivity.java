defd3f7ad0dbdf9b2de5fff6167a1a9e9be91940
lunolabs <pashtet300795@gmail.com>
2015-07-15 17:47:34 +0300

    Initial commit

---
+++ app/src/main/java/com/whitebrown/osmdoner/MainActivity.java
@@ -0,0 +1,25 @@
+package com.whitebrown.osmdoner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(11);
+        GeoPoint startPoint = new GeoPoint(60.15, 30.05);
+        mapController.setCenter(startPoint);
+    }
+}
